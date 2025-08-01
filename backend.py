3e853f90598e47b8474c1db391f046d0fbcd40f0
rameel ahmed <rameelahmed786@gmail.com>
2025-08-01 09:15:35 -0400

    get data from api

--- backend.py
+++ backend.py
@@ -7,6 +7,7 @@ def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
+    print(data)
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
